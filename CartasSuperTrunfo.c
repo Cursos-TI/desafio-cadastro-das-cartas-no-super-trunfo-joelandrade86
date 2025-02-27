746a6f82a81a5a56434d8e5755de9cf66f4f1e93
joelandrade86 <joel.ilhadigital@gmail.com>
2025-02-27 01:02:08 +0000

    calculo divisão

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,35 +2,42 @@
 
 
 
-int main() {                            // Carta 1 e 2
-   printf("\n");
+int main() {                            //informações salvas da Carta 1 e 2
+   
     //carta 1
-    char estado2 = 'B';                   
-    char estado = 'A';                    
+    char estado2 = 'B';                   // Aqui eu insiro as variáveis das duas cartas, dou um espaçamento para ficar menos bagunçado e mais legível.
+    char estado = 'A';                    // Estado sendo representado por uma letra de A a H.
                                           
-    char codigo2[5] = "B02";               
+    char codigo2[5] = "B02";               // Código vai ser a letra escolhida para o estado + um número de 01 a 04. ex: A01, B02 ..
     char codigo[5] = "A01";
     
-    char nome2[20]  = "Fortaleza";            
+    char nome2[20]  = "Fortaleza";            // Nome das cidades.
     char nome[20] = "Rio de Janeiro"; 
     
-    float area2 = 312.4;                                
-    float area = 43.750; 
+    float area = 312.400;                  //Área em km² usando float .              
+    float area2 = 43.750; 
 
-    float pib = 73;                   
+    float pib = 73;                   // PIB (produto interno bruto) usando float.
     float pib2= 394;
+
+    float divis, divis2, divis3, divis4;    // variavel da divisão
                                        
-    int pontosturisticos= 15;             
+    int pontosturisticos= 15;             // Quantidades de pontos turísticos das cidades.
     int pontosturisticos2= 20;
     
-    int populacao = 6200000;            
+    int populacao = 6200000;            //População das cidades (os dados sempre mudam, é apenas uma estimativa da quantidade de pessoas).
     int populacao2= 2400000;   
 
-    printf("Cartas Super Trunfo\n");                       
+    divis = populacao / area;           // divisão da população + area
+    divis2 = populacao / pib;           // divisão da população + pib
+    divis3 = populacao2 / area2;        // divisão da população + area
+    divis4  = populacao2 / pib2;        // divisão da população + pib
+    
+    printf("Cartas Super Trunfo\n");                       //Print F com todas as informações das cartas
     printf("Carta 1\n");                             
     printf("Estado: %c\n", estado);
     
-    printf("Nome da Cidade: %s\n", nome);                  
+    printf("Nome da Cidade: %s\n", nome);                  // Variáveis com seus demais especificadores !
    
     printf("Código: %s\n", codigo);
      
@@ -41,10 +48,17 @@ int main() {                            // Carta 1 e 2
     printf("População: %d\n", populacao);
 
     printf ("Pontos Turísticos: %d\n", pontosturisticos);
-     
+
+    printf("Densidade Populacional: %.fkm²\n", divis);
+
+    printf("PIB per Capita: %.f mil\n", divis2);
+
     printf("\n");
+     
+
     
     printf("Carta 2\n");
+
     printf("Estado: %c\n", estado2);
     
     printf("Nome da Cidade: %s\n", nome2);
@@ -58,5 +72,11 @@ int main() {                            // Carta 1 e 2
     printf("População: %d\n", populacao2);
 
     printf ("Pontos Turísticos: %d\n", pontosturisticos2);
+
+    printf("Densidade Populacional: %.fkm²\n", divis3);
+
+    printf("PIB per Capita: %.f mil\n", divis4);
+
+    return 0;
 
 }  
